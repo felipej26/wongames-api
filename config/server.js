661c045b52e2f2d0f7e1fc16dfc9dc96f0792d3d
Jefferson Felipe <felipej2626@gmail.com>
2021-04-29 03:34:20 -0300

    Criação do projeto

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '545d38732a35c34ab8f78afdb918a674'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6aadd95d-c7fe-4dca-82de-c7d822160655'
+};
